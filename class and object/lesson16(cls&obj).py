5acc039b3f32eaf4e2753579e95baf69cb139da2
unknown <sivamaluventhi@gmail.com>
2026-05-15 13:28:26 +0530

    added loops notes

---
+++ class and object/lesson16(cls&obj).py
@@ -0,0 +1,29 @@
+#class and object under the oops concepts
+#class is kind of classroom like having many objects like student and table,board,etc..
+#if we use def inside the class is know as method ,if outside the class its known as function.
+ # ex
+"""
+class student:
+    def say_hello(self): # here using self is rule 
+        print("hi,im a student!")
+s1=student() #here this is a caller to call the method to execute 
+s1.say_hello() #here we call the method using the caller s1
+#here this is how python understand s1.say_hello(s1),calling from s1 
+"""
+#another ex
+class student:
+    def __init__(self, name, grade):
+        self.name =  name
+        self.grade = grade
+
+    def display(self):
+         print(f"{self.name} is in grade{self.grade}")
+
+#using multiple subject
+s1=student(name= "abi",grade= 1)
+s2=student(name="aj",grade=2)
+s3=student(name="sar",grade=3)
+
+s1.display()
+s2.display()
+s3.display()
